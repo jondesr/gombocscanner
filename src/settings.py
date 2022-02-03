e950319ed405b409a1d14095f2928c18b4a45bcb
Jonathan Desrocher <jonathan@gomboc.ai>
2022-02-03 10:59:57 -0800

    Updated settings

--- src/settings.py
+++ src/settings.py
@@ -1,11 +1,18 @@
 from __future__ import annotations
+from enum import Enum
 from pydantic import BaseSettings
+
+class DataSource(Enum):
+    NEO4J = "neo4j"
+    PICKLE = "pickle"
 
 class Settings(BaseSettings):
     class Config:
         env_file = '.env'
         env_file_encoding = 'utf-8'
+        use_enum_values = True
 
+    DATASOURCE: DataSource = DataSource.PICKLE
     NEO4J_URL: str
     NEO4j_USER: str
     NEO4j_PASSWORD: str
